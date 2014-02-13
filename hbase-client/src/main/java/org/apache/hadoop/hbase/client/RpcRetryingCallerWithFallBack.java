1c55a10e083c169bae6179e8bc477d487a6563df
Nicolas Liochon <nkeywal@gmail.com>
2014-02-13 02:47:17 +0100

    full client

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/RpcRetryingCallerWithFallBack.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/RpcRetryingCallerWithFallBack.java
@@ -1,3 +1,23 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package org.apache.hadoop.hbase.client;
 
 
@@ -157,7 +177,7 @@ public class RpcRetryingCallerWithFallBack {
     addCallsForReplica(cs, inProgress, rl, 0, 0); // primary.
 
     try {
-      Future<Result> f = cs.poll(1, TimeUnit.MICROSECONDS); // Yes, microseconds
+      Future<Result> f = cs.poll(timeBeforeReplicas, TimeUnit.MICROSECONDS); // Yes, microseconds
       if (f == null) {
         addCallsForReplica(cs, inProgress, rl, 1, rl.size() - 1);  // secondaries
         f = cs.take();
