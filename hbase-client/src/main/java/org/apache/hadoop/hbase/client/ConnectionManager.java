mapr/hbase-old

HBASE-10630 NullPointerException in ConnectionManager$HConnectionImplementation.locateRegionInMeta() due to missing region info
git-svn-id: https://svn.apache.org/repos/asf/hbase/branches/hbase-10070@1572761 13f79535-47bb-0310-9956-ffa450edef68

hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionManager.java
@@ -1162,6 +1162,10 @@ class ConnectionManager {
 
           // convert the row result into the HRegionLocation we need!
           location = MetaReader.getRegionLocations(regionInfoRow);
+          if (location == null || location.getRegionLocation() == null) {
+            throw new IOException("HRegionInfo was null in " +
+              parentTable + ", row=" + regionInfoRow);
+          }
           HRegionInfo regionInfo = location.getRegionLocation().getRegionInfo();
           if (regionInfo == null) {
             throw new IOException("HRegionInfo was null or empty in " +

enis/hbase

hbase-client/src/main/java/org/apache/hadoop/hbase/client/ConnectionManager.java
@@ -1065,6 +1065,10 @@ class ConnectionManager {
 
           // convert the row result into the HRegionLocation we need!
           RegionLocations locations = MetaReader.getRegionLocations(regionInfoRow);
+          if (locations == null || locations.getRegionLocation() == null) {
+            throw new IOException("HRegionInfo was null in " +
+              tableName + ", row=" + regionInfoRow);
+          }
           HRegionInfo regionInfo = locations.getRegionLocation().getRegionInfo();
           if (regionInfo == null) {
             throw new IOException("HRegionInfo was null or empty in " +